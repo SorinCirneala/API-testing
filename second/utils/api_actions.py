7e8c209f742326e8cd26b34afa3570cd94767e6c
Sorin <sorin.cirneala@gmail.com>
2023-05-20 14:52:00 +0300

    added more action methods

--- second/utils/api_actions.py
+++ second/utils/api_actions.py
@@ -5,7 +5,69 @@ sys.path.insert(0, project_path)
 import requests
 from requests import Response
 from utils.endpoints import carts_url, carts_url, orders_url, products_url, register_url, status_url
+from utils.secret import access_token
+
+# TODO remove after debugging
+from utils.secret import mock_cart_id
 
 def get_status() -> Response:
-    response = requests.get(status_url)
-    return response
+    return requests.get(status_url)
+
+
+def get_all_products() -> Response:
+    return requests.get(products_url)
+
+
+def get_product_by_id(product_id, label=None) -> Response:
+    """ 
+    product_id (int)
+    label (boolean)
+    """
+    payload = {"product-label": label}
+    url = f"{products_url}/{product_id}"
+    return requests.get(url, params=payload)
+
+
+def create_cart() -> Response:
+    return requests.post(carts_url)
+
+
+def get_cart_info(cart_id) -> Response:
+    """
+    cart_id (string)
+    """
+    url = f"{carts_url}/{cart_id}"
+    return requests.get(url) 
+
+
+def get_cart_items(cart_id) -> Response:
+    """
+    cart_id (string)
+    """
+    url = f"{carts_url}/{cart_id}/items"
+    return requests.get(url)
+
+
+def add_item_to_cart(cart_id, item_id) -> Response:
+    """
+    cart_id (string)
+    item_id (int)
+    """
+    url = f"{carts_url}/{cart_id}/items"
+    payload = {
+        "productId": str(item_id)
+    }
+    return requests.post(url, json=payload)
+
+
+
+
+
+print(get_cart_items(mock_cart_id).json())
+# print(add_item_to_cart(mock_cart_id, 5478).json())
+
+# for debugging
+# response = requests.get(url, params=payload)
+# print(response.request.url)
+# print(response.request.body)
+# print(response.request.headers)
