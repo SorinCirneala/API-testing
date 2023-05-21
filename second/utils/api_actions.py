d7ab5a7517f5d47d17341132d40da42fec9b11eb
Sorin <sorin.cirneala@gmail.com>
2023-05-21 19:05:52 +0300

    added order-related methods

--- second/utils/api_actions.py
+++ second/utils/api_actions.py
@@ -5,7 +5,7 @@ sys.path.insert(0, project_path)
 import requests
 from requests import Response
 from utils.endpoints import carts_url, carts_url, orders_url, products_url, register_url, status_url
-from utils.secret import access_token
+from utils.secret import ACCESS_TOKEN
 
 # TODO remove after debugging
 from utils.secret import mock_cart_id, mock_item_id
@@ -30,7 +30,7 @@ def get_product_by_id(product_id: int, label: bool=None) -> Response:
 
 
 def create_cart() -> Response:
-    """ Returns response object with cart details in JSON format """
+    """ Returns response object with created confirmation and cart ID in JSON format """
     return requests.post(carts_url)
 
 
@@ -80,10 +80,67 @@ def delete_item_in_cart(cart_id: str, item_id: int) -> Response:
     return requests.delete(url)
 
 
+def create_order(cart_id: str, customer: str, comment:str = None) -> Response:
+    """ Returns response object with created confirmation and order ID in JSON format """
+    url = f"{orders_url}"
+    headers = {
+        "Authorization": ACCESS_TOKEN
+    }
+    payload = {
+        "cartId": cart_id,
+        "customerName": customer,
+        "comment": comment
+    }
+    return requests.post(url, headers=headers, json=payload)
+
+
+def get_all_orders() -> Response:
+    """ Returns response object with an array of JSON objects (orders) """
+    url = f"{orders_url}"
+    headers = {
+        "Authorization": ACCESS_TOKEN
+    }
+    return requests.get(url, headers=headers)
+
+
+def get_order_info(order_id: str, invoice: bool = None) -> Response:
+    """ Returns response object with the order details (JSON objects) """
+    url = f"{orders_url}/{order_id}"
+    headers = {
+        "Authorization": ACCESS_TOKEN
+    }
+    query_params = {
+        "invoice": invoice
+    }
+    return requests.get(url, headers=headers, params=query_params)
+
+
+def update_order(order_id: str, customer: str = None, comment:str = None) -> Response:
+    """ Returns response object with no text """
+    url = f"{orders_url}/{order_id}"
+    headers = {
+        "Authorization": ACCESS_TOKEN
+    }
+    payload = {
+        "customerName": customer,
+        "comment": comment
+    }
+    return requests.patch(url, headers=headers, json=payload)
+
+
+def delete_order(order_id: str) -> Response:
+    """ Returns response object with no text """
+    url = f"{orders_url}/{order_id}"
+    headers = {
+        "Authorization": ACCESS_TOKEN
+    }
+    return requests.delete(url, headers=headers)
+
+
 # TODO remove after debugging
-print(get_cart_info(mock_cart_id).text)
-# response = delete_item_in_cart(mock_cart_id, 153502116)
+# response = delete_order("G97iCYousqdKJgv50eWaX")
 # print(response.text)
 # print(response.request.url)
 # print(response.request.body)
 # print(response.request.headers)
+# print(get_order_info("G97iCYousqdKJgv50eWaX").text)
