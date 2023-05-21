6dc607fd003233087bb46e2a244cb8c29f321a93
Sorin <sorin.cirneala@gmail.com>
2023-05-21 16:51:57 +0300

    refactored comments and added parameter hints

--- second/utils/api_actions.py
+++ second/utils/api_actions.py
@@ -10,49 +10,44 @@ from utils.secret import access_token
 # TODO remove after debugging
 from utils.secret import mock_cart_id, mock_item_id
 
+
+# api actions
 def get_status() -> Response:
+    """ Returns response object with API status """
     return requests.get(status_url)
 
 
 def get_all_products() -> Response:
+    """ Returns response object with list of products in JSON format """
     return requests.get(products_url)
 
 
-def get_product_by_id(product_id, label=None) -> Response:
-    """ 
-    product_id (int)
-    label (boolean)
-    """
+def get_product_by_id(product_id: int, label: bool=None) -> Response:
+    """ Returns response object with product details in JSON format """
     payload = {"product-label": label}
     url = f"{products_url}/{product_id}"
     return requests.get(url, params=payload)
 
 
 def create_cart() -> Response:
+    """ Returns response object with cart details in JSON format """
     return requests.post(carts_url)
 
 
-def get_cart_info(cart_id) -> Response:
-    """
-    cart_id (string)
-    """
+def get_cart_info(cart_id: str) -> Response:
+    """ Returns response object with cart details in JSON format """
     url = f"{carts_url}/{cart_id}"
     return requests.get(url) 
 
 
-def get_cart_items(cart_id) -> Response:
-    """
-    cart_id (string)
-    """
+def get_cart_items(cart_id: str) -> Response:
+    """ Returns response object with list of items added to cart in JSON format """
     url = f"{carts_url}/{cart_id}/items"
     return requests.get(url)
 
 
-def add_item_to_cart(cart_id, product_id) -> Response:
-    """
-    cart_id (string)
-    product_id (int)
-    """
+def add_item_to_cart(cart_id: str, product_id: int) -> Response:
+    """ Returns response object with add confirmation in JSON format """
     url = f"{carts_url}/{cart_id}/items"
     payload = {
         "productId": f"{product_id}"
@@ -60,12 +55,8 @@ def add_item_to_cart(cart_id, product_id) -> Response:
     return requests.post(url, json=payload)
 
 
-def modify_item_in_cart(cart_id, item_id, quantity) -> Response:
-    """
-    cart_id (string)
-    item_id (int)
-    quantity (int)
-    """
+def modify_item_in_cart(cart_id: str, item_id: int, quantity: int) -> Response:
+    """ Returns response object with no text """
     url = f"{carts_url}/{cart_id}/items/{item_id}"
     payload = {
         "quantity": f"{quantity}"
@@ -73,13 +64,8 @@ def modify_item_in_cart(cart_id, item_id, quantity) -> Response:
     return requests.patch(url, json=payload)
 
 
-def replace_item_in_cart(cart_id, item_id, product_id, quantity) -> Response:
-    """
-    cart_id (string)
-    item_id (int)
-    product_id (int)
-    quantity (int)
-    """
+def replace_item_in_cart(cart_id: str, item_id: int, product_id: int, quantity: int) -> Response:
+    """ Returns response object with no text """
     url = f"{carts_url}/{cart_id}/items/{item_id}"
     payload = {
         "productId": f"{product_id}",
@@ -88,11 +74,8 @@ def replace_item_in_cart(cart_id, item_id, product_id, quantity) -> Response:
     return requests.put(url, json=payload)
 
 
-def delete_item_in_cart(cart_id, item_id) -> Response:
-    """
-    cart_id (string)
-    item_id (int)
-    """
+def delete_item_in_cart(cart_id: str, item_id: int) -> Response:
+    """ Returns response object with no text """
     url = f"{carts_url}/{cart_id}/items/{item_id}"
     return requests.delete(url)
 
