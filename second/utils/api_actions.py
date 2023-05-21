743f0629dbeba17fdcd44f0e957536f262a8e4d9
Sorin <sorin.cirneala@gmail.com>
2023-05-21 16:22:09 +0300

    added two new methods

--- second/utils/api_actions.py
+++ second/utils/api_actions.py
@@ -8,7 +8,7 @@ from utils.endpoints import carts_url, carts_url, orders_url, products_url, regi
 from utils.secret import access_token
 
 # TODO remove after debugging
-from utils.secret import mock_cart_id
+from utils.secret import mock_cart_id, mock_item_id
 
 def get_status() -> Response:
     return requests.get(status_url)
@@ -48,21 +48,36 @@ def get_cart_items(cart_id) -> Response:
     return requests.get(url)
 
 
-def add_item_to_cart(cart_id, item_id) -> Response:
+def add_item_to_cart(cart_id, product_id) -> Response:
     """
     cart_id (string)
-    item_id (int)
+    product_id (int)
     """
     url = f"{carts_url}/{cart_id}/items"
     payload = {
-        "productId": str(item_id)
+        "productId": str(product_id) 
     }
     return requests.post(url, json=payload)
 
 
+def modify_item_in_cart(cart_id, item_id, quantity) -> Response:
+    """
+    cart_id (string)
+    item_id (int)
+    quantity (int)
+    """
+    url = f"{carts_url}/{cart_id}/items/{item_id}"
+    payload = {
+        "quantity": f"{quantity}"
+    }
+    return requests.patch(url, json=payload)
+
+
+
 # TODO remove after debugging
-response = add_item_to_cart(mock_cart_id, 5478)
-print(response.text)
-print(response.request.url)
-print(response.request.body)
-print(response.request.headers)
+# print(get_cart_info(mock_cart_id).text)
+# response = modify_item_in_cart(mock_cart_id, mock_item_id, 3)
+# print(response.text)
+# print(response.request.url)
+# print(response.request.body)
+# print(response.request.headers)
