6115e7bb560da371a325aa262680fb816a72bbc1
Sorin <sorin.cirneala@gmail.com>
2023-05-22 18:02:51 +0300

    added more tests

--- grocery-store-api/tests/products/test_product_list.py
+++ grocery-store-api/tests/products/test_product_list.py
@@ -4,6 +4,7 @@ sys.path.insert(0, project_path)
 
 import utils.api_actions as api
 
+
 def test_products_list_is_returned():
     """ Check if the list of products is returned and contains products """
     response = api.get_all_products()
@@ -53,3 +54,46 @@ def test_product_list_only_sold_out():
     assert response.status_code == 200, "Status code should be 200"
     for item in response.json():
         assert item["inStock"] == False, "Product is not sold out"
+
+
+def test_product_list_wrong_available_value():
+    """ Check error when using wrong input for 'results' parameter """
+    response = api.get_all_products(is_available="wrong")
+    assert response.status_code == 400, "Status code should be 400"
+    assert "Must be one of: true, false" in response.json()["error"], "Incorrect error message"
+
+
+def test_product_list_wrong_category():
+    """ Check error when requesting wrong category """
+    category = "wrong"
+    response = api.get_all_products(category=category)
+    assert response.status_code == 400, "Status code should be 400"
+    assert "Must be one of: meat-seafood, fresh-produce, candy, bread-bakery, dairy, eggs, coffee" in response.json()["error"], "Incorrect error message"
+
+
+def test_product_list_results_less_than_zero():
+    """ Check error when requesting negative values for results """
+    response = api.get_all_products(max_results = -1)
+    assert response.status_code == 400, "Status code should be 400"
+    assert "Must be greater than 0." in response.json()["error"], "Incorrect error message"
+
+
+def test_product_list_results_more_than_twenty():
+    """ Check error when requesting more than default value (20) """
+    response = api.get_all_products(max_results = 21)
+    assert response.status_code == 400, "Status code should be 400"
+    assert "Cannot be greater than 20." in response.json()["error"], "Incorrect error message"
+
+
+def test_product_list_results_wrong_value():
+    # Expected to fail, check issue #1
+    """ Check error when using wrong input for 'results' parameter """
+    response = api.get_all_products(max_results = "five")
+    assert response.status_code == 400, "Status code should be 400"
+    assert "Invalid value for query parameter 'results'" in response.json()["error"], "Incorrect error message"
+
+
+# TODO: add more test cases
+""" combine category with results """
+""" combine category with available """
+""" combine results with available """
